16602c941e314cba85d14da1439fb0feedb41d10
Dmitry Ilyin <dima@doty.ru>
2021-03-04 20:50:08 +0300

    Optimize bin2sh algorithm

--- bin2sh.c
+++ bin2sh.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+const char *trtable[256] = {
+    "\\0", // 0
+    NULL,  // 1
+    NULL,  // 2
+    NULL,  // 3
+    NULL,  // 4
+    NULL,  // 5
+    NULL,  // 6
+    "\\a", // 7
+    "\\b", // 8
+    "\\t", // 9
+    "\\n", // 10
+    "\\v", // 11
+    "\\f", // 12
+    "\\r", // 13
+};
+
+int printable(char ch) {
+  return ch != '`' && ch != '"' && ch != '\\' && (ch >= 'a' && ch <= 'z');
+}
+
+#define SHELL_INPUT_MAX 700
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     printf("Usage: %s binary > textfile\n", argv[0]);
@@ -13,24 +36,32 @@ int main(int argc, char **argv) {
     exit(EXIT_FAILURE);
   }
 
-  unsigned char buf[250];
-  int nlen;
+  unsigned char byte;
 
   const char *filename = argv[1];
-  int line = 0;
+  int line = 0, chout = 0;
   const char *newf = ">";
   const char *exst = ">>";
-  //fprintf(stdout, "rm %s; while IFS= read -s -r line; do $line>>%s; done\n",
-  //        filename, filename);
 
   fprintf(stdout, "cd /tmp;F=%s;true>$F;chmod +x $F\n", filename);
 
-  while ((nlen = fread(buf, 1, sizeof(buf), f))) {
-    line++;
-    fprintf(stdout, "printf \"");
-    for (int i = 0; i < nlen; i++) {
-      fprintf(stdout, "\\x%02X", buf[i]);
+  while (fread(&byte, 1, sizeof(byte), f)) {
+    if (chout == 0) {
+      chout += fprintf(stdout, "printf \"");
+      line++;
     }
+    if (byte == '"')
+      chout += fprintf(stdout, "\"");
+    else if (trtable[byte])
+      chout += fprintf(stdout, "%s", trtable[byte]);
+    else
+      chout += fprintf(stdout, "\\x%X", byte);
+    if (chout > SHELL_INPUT_MAX) {
+      fprintf(stdout, "\"%s%s\n", line == 1 ? newf : exst, "$F");
+      chout = 0;
+    }
+  }
+  if (chout) {
     fprintf(stdout, "\"%s%s\n", line == 1 ? newf : exst, "$F");
   }
   fprintf(stdout, "\n");
